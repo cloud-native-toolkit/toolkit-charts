7fcc46f1a0b33c95efe46ed3e5e2a9d924b34813
Sean Sundberg <seansund@us.ibm.com>
2020-02-24 15:59:52 -0600

    Updates base repo url for helm repo

--- scripts/packge-helm.sh
+++ scripts/packge-helm.sh
@@ -5,6 +5,7 @@ ROOT_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
 
 TMP_DIR="${ROOT_DIR}/tmp"
 CHART_DIR="${ROOT_DIR}/stable"
+BASE_REPO_URL="https://ibm-garage-cloud.github.io/toolkit-charts/"
 
 mkdir -p "${TMP_DIR}"
 
@@ -21,7 +22,7 @@ git checkout gh-pages
 
 cp ${TMP_DIR}/* "${CHART_DIR}"
 
-helm repo index "${CHART_DIR}" --url "https://ibm-garage-cloud.github.io/catalyst-charts/stable/" --merge "${CHART_DIR}/index.yaml"
+helm repo index "${CHART_DIR}" --url "${BASE_REPO_URL}/stable/" --merge "${CHART_DIR}/index.yaml"
 cp "${CHART_DIR}/index.yaml" "${ROOT_DIR}"
 
 rm -rf "${TMP_DIR}"
