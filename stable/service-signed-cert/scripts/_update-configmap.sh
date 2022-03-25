d8ac732a956d41ed7938243bc6125d49d9916a8a
Sean Sundberg <seansund@us.ibm.com>
2022-03-24 22:48:34 -0500

    service-signed-cert: v0.2.2 (#318)
    - Extracts shell scripts into separate files so they can be tested independently
    - Adds logic to generate configmap from shell scripts
    - Updates shell scripts from testing
    
    Closes #317
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ stable/service-signed-cert/scripts/_update-configmap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+SUPPORT_DIR=$(cd "${SCRIPT_DIR}/../support"; pwd -P)
+TEMPLATE_DIR=$(cd "${SCRIPT_DIR}/../templates"; pwd -P)
+
+if ! command -v oc 1> /dev/null 2> /dev/null; then
+  echo "oc cli required" >&2
+  exit 1
+fi
+
+if ! command -v yq 1> /dev/null 2> /dev/null; then
+  echo "yq cli required" >&2
+  exit 1
+fi
+
+cat "${SUPPORT_DIR}/configmap.snippet.yaml" > "${TEMPLATE_DIR}/configmap.yaml"
+
+oc create configmap tmp \
+  --from-file=${SCRIPT_DIR}/approve-csr.sh \
+  --from-file=${SCRIPT_DIR}/create-csr.sh \
+  --from-file=${SCRIPT_DIR}/create-secret.sh \
+  --from-file=${SCRIPT_DIR}/wait-for-csr.sh \
+  --dry-run=client \
+  -o yaml | \
+yq eval 'del(.apiVersion) | del(.kind) | del(.metadata)' - >> "${TEMPLATE_DIR}/configmap.yaml"
